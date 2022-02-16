c203b7f464e506b5b14b98ba98afb0c0b5a8249c
Rizkiananda Pradillah <rpradillah@gmail.com>
2022-02-16 18:31:44 +0700

    add soal nomor 1

---
+++ NawaDataLogic.App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NawaDataLogic.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "Y";
+
+            while (answer.ToUpper() == "Y")
+            {
+                try
+                {
+                    Console.WriteLine("Please enter test number : ");
+                    Console.WriteLine("1. Sorting Character");
+                    Console.WriteLine("2. PSBB (Pembatasan Sosial Berskala Besar)");
+                    Console.WriteLine("\n");
+                    int iTest = int.Parse(Console.ReadLine());
+
+                    switch (iTest)
+                    {
+                        case 1:
+                            Test01 test01 = new Test01();
+                            break;
+                        default:
+                            Console.WriteLine("Test is not available.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("\n");
+                Console.WriteLine("Continue ? (y/n)");
+                answer = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ NawaDataLogic.App/Test01.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NawaDataLogic.App
+{
+    class Test01
+    {
+        public Test01()
+        {
+            Console.WriteLine("Input one line of words :");
+            string sWords = Console.ReadLine();
+
+            Result(sWords.ToLower());
+        }
+
+        // STRING SORTING
+        public string sortString(string word)
+        {
+            char[] result = word.ToArray();
+            Array.Sort(result);
+            return new string(result);
+        }
+
+        public void Result(string sWords)
+        {
+            // VOWEL VARIABLE
+            string aVowel = "aiueo";
+            string resultVowel = string.Empty;
+            string resultConsonant = string.Empty;
+
+            for (int i = 0; i < sWords.Length; i++)
+            {
+                // IGNORE WHITE SPACE
+                if (sWords[i].ToString() != " ")
+                {
+                    var sTemp = string.Empty;
+                    for (int j = 0; j < aVowel.Length; j++)
+                    {
+                        if (sWords[i] == aVowel[j])
+                        {
+                            resultVowel += sWords[i];
+                            sTemp = string.Empty;
+                            break;
+                        }
+                        else
+                        {
+                            sTemp = sWords[i].ToString();
+                        }
+                    }
+                    resultConsonant += sTemp;
+                }
+            }
+
+            Console.WriteLine("Vowel Characters : " + sortString(resultVowel));
+            Console.WriteLine("Consonant Characters : " + sortString(resultConsonant));
+        }
+    }
+}
